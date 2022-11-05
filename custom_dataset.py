a696a42db826e8f3e6b36f17a3316552d2c6b029
ZjTan4 <ztan4@ualberta.ca>
2022-11-05 16:52:04 -0600

    something

---
+++ custom_dataset.py
@@ -0,0 +1,88 @@
+import imageio
+from pathlib import Path
+import torch
+
+
+
+class CustomDatsetMemory(torch.utils.data.Dataset):
+    def __init__(self, root=None, img_transform=None, gt_transform=None, img_ext='.jpg', gt_ext='.png'):
+        self.root = root
+        self.img_transform = img_transform
+        self.gt_transform = gt_transform
+
+        image_dir = Path('{}/images'.format(self.root))
+        images = sorted(list(image_dir.glob('*{}'.format(img_ext))))
+        mask_dir = Path('{}/masks'.format(self.root))
+        masks = sorted(list(mask_dir.glob('*{}'.format(gt_ext))))
+
+        assert self.root is not None
+        assert len(images) == len(masks) and len(images) > 0
+        assert all(images[i].stem == masks[i].stem for i in range(len(images))) 
+
+        self.images = read_images(images)
+        self.masks = read_images(masks)
+
+    def __getitem__(self, index):
+        image = self.images[index]
+        mask = self.masks[index]
+
+        if self.img_transform is not None:
+            image = self.img_transform(image)
+        if self.gt_transform is not None:
+            mask = self.gt_transform(mask)
+
+        return image, mask
+
+    def __len__(self):
+        return len(self.images)
+
+class CustomDatsetIO(torch.utils.data.Dataset):
+    def __init__(self, root=None, img_transform=None, gt_transform=None, img_ext='.jpg', gt_ext='.png'):
+        self.root = root
+        self.img_transform = img_transform
+        self.gt_transform = gt_transform
+
+        image_dir = Path('{}/images'.format(self.root))
+        images = sorted(list(image_dir.glob('*{}'.format(img_ext))))
+        mask_dir = Path('{}/masks'.format(self.root))
+        masks = sorted(list(mask_dir.glob('*{}'.format(gt_ext))))
+
+        assert self.root is not None
+        assert len(images) == len(masks) and len(images) > 0
+        assert all(images[i].stem == masks[i].stem for i in range(len(images))) 
+
+        self.images = images
+        self.masks = masks
+
+    def __getitem__(self, index):
+        image = torch.tensor(imageio.imread(str(self.images[index])))
+        mask = torch.tensor(imageio.imread(str(self.masks[index])))
+
+        if self.img_transform is not None:
+            image = self.img_transform(image)
+        if self.gt_transform is not None:
+            mask = self.gt_transform(mask)
+
+        return image, mask
+
+    def __len__(self):
+        return len(self.images)
+
+def read_images(image_paths):
+    frame = len(image_paths)
+    im = torch.tensor(imageio.imread(str(image_paths[0])))
+    byte = 1
+    if len(im.shape) == 3:
+        row, column, byte = im.shape
+    else:
+        row, column = im.shape
+
+    images = torch.empty([frame, row, column, byte], dtype=im.dtype).squeeze()
+    # print("loading images into memory")
+    for i in range(frame):
+        print("loading {} file: {}".format(i + 1, image_paths[i].stem), end = '\r')
+        im = torch.tensor(imageio.imread(str(image_paths[i])))
+        images[i] = im
+    print("")
+    # print("loaded {} frames into memory".format(frame))
+    return images
--- main.py
+++ main.py
@@ -1,31 +1,48 @@
+from datetime import datetime
 import os
 import torch
 import torchvision.transforms as transforms
 import torchvision.datasets as dset
+
+from custom_dataset import CustomDatsetMemory, CustomDatsetIO
 
 import matplotlib.pyplot as plt
 
 if not os.path.exists('./model'):
     os.mkdir('./model')
 
-us_cuda = True if torch.cuda.is_available() else False
-if us_cuda:
+use_cuda = True if torch.cuda.is_available() else False
+if use_cuda:
     print('use cuda')
 else:
     print('cpu')
 device = torch.device("cuda:9" if use_cuda else "cpu")
 
-data_path = "./data"
-batch_size = 200
+data_path = "./data/bdd10k"
+batch_size = 50
 transform = transforms.Compose([
     transforms.ToTensor(), 
 ])
-dataset = dset.ImageFolder(data_path, transform=transform)
+
+# dataset = CustomDatsetMemory(data_path)
+dataset = CustomDatsetIO(data_path)
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, drop_last=True, shuffle=True)
-
-
 
 num_epoch = 100
 for epoch in range(num_epoch):
-    for i, (img, _) in enumerate(dataloader):
-        pass
+    start = datetime.now()
+    for i, (img, mask) in enumerate(dataloader):
+        print(img.shape)
+        img = img.to(device)
+        mask = mask.to(device)
+        # tmp = img
+        # plt.imshow(tmp[0])
+        # plt.show()
+        # plt.imshow(tmp[1])
+        # plt.show()
+        # plt.imshow(mask[0])
+        # plt.show()
+        # plt.imshow(mask[1])
+        # plt.show()
+        print(datetime.now() - start)
+        start = datetime.now()
